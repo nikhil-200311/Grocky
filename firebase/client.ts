8e39a8c1896ece09b4235d8b87b768d5ad893a82
Nikhil Kumar Agarwal K <harshita10203040@gmail.com>
2025-05-02 03:21:14 +0530

    first commit

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
@@ -20,7 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
